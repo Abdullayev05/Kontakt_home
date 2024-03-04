dc1c83caaafd281f428338cb850d2efdd1258307
Abdullayev05 <abdullayevmahammadaga@gmail.com>
2024-03-05 02:42:16 +0400

    kontakt_home

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,27 @@
 function toggleRedHeart(event) {
-    event.preventDefault(); // Bağlantının varsayılan davranışını engelle
-    var icon = event.currentTarget.querySelector('.fa-heart'); // Bağlantı içindeki ikonu al
-    icon.classList.toggle('clicked'); // 'clicked' sınıfını ekle/çıkar
+    event.preventDefault();
+    var icon = event.currentTarget.querySelector('.fa-heart');
+    icon.classList.toggle('clicked');
+}
+
+function togglePopUp() {
+    var popUp = document.getElementById("popUp");
+    if (popUp.style.display === "block") {
+        popUp.style.display = "none";
+    } else {
+        popUp.style.display = "block";
+    }
+}
+
+function togglePopUp() {
+    var popUp = document.getElementById("popUp");
+    popUp.classList.toggle("active");
+}
+
+document.getElementById("closeIcon").addEventListener("click", function () {
+    document.querySelector(".pop_up").style.display = "none";
+});
+
+function toggleRedHeart(event) {
+    event.preventDefault();
 }
