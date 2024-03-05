7411c106ebc5b38c480c266f1a0377152ce1f7fa
Abdullayev05 <abdullayevmahammadaga@gmail.com>
2024-03-05 10:51:03 +0400

    kontakt_home

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -13,3 +13,54 @@ document.getElementById("krediAy").addEventListener("input", function () {
 function toggleRedHeart(element) {
   element.classList.toggle('clicked');
 }
+function togglePopUp() {
+  var popUp = document.getElementById("popUp");
+  if (popUp.style.display === "block") {
+      popUp.style.display = "none";
+  } else {
+      popUp.style.display = "block";
+  }
+}
+
+function togglePopUp() {
+  var popUp = document.getElementById("popUp");
+  popUp.classList.toggle("active");
+}
+document.getElementById("closeIcon").addEventListener("click", function () {
+  document.querySelector(".pop_up").style.display = "none";
+})
+function toggleRedHeart(event) {
+  event.preventDefault();
+}
+function togglePasswordVisibility(event) {
+  event.preventDefault();
+  let passwordField = document.getElementById("password");
+  let toggleButton = document.getElementById("toggleButton");
+  if (passwordField.type === "password") {
+      passwordField.type = "text";
+      toggleButton.textContent = "Hide";
+  } else {
+      passwordField.type = "password";
+      toggleButton.textContent = "Show";
+  }
+}
+function login(event) {
+  event.preventDefault();
+}
+
+function signUp(event) {
+  event.preventDefault();
+}
+$('.memory-button').click(function() {
+  // Tüm butonların arka plan rengini ve kenarlık rengini varsayılan hale getir
+  $('.memory-button').css({
+      'background-color': '#FFFFFF',
+      'border': '1px solid #E7E7E7'
+  });
+
+  // Tıklanan butonun arka plan rengini ve kenarlık rengini değiştir
+  $(this).css({
+      'background-color': '#b9bbbd',
+      'border': '1px solid #F58634'
+  });
+});
