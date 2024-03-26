f194b40c6f7bb560482335b731c17cfb05a640c2
Abdullayev05 <abdullayevmahammadaga@gmail.com>
2024-03-26 05:04:20 +0400

    kontakt_home

---
+++ assets/js/basket.js
@@ -0,0 +1,17 @@
+function updateClock() {
+    const now = new Date();
+    const hour = now.getHours().toString().padStart(2, '0');
+    const minute = now.getMinutes().toString().padStart(2, '0');
+    const second = now.getSeconds().toString().padStart(2, '0');
+  
+    document.getElementById('hour').textContent = hour;
+    document.getElementById('minute').textContent = minute;
+    document.getElementById('second').textContent = second;
+  }
+  
+  // Update the clock every second
+  setInterval(updateClock, 1000);
+  
+  // Initial call to display the clock immediately
+  updateClock();
+  
