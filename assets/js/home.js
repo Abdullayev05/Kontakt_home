16ea9da22ea0d99bbcbb43d9229ccb703c283887
Abdullayev05 <abdullayevmahammadaga@gmail.com>
2024-06-24 21:45:44 +0400

    Kontakt_Home

--- assets/js/home.js
+++ assets/js/home.js
@@ -95,28 +95,3 @@ $('.satis_lideri button').click(function () {
 document.getElementById('video_link').addEventListener('click', function () {
   window.open('https://youtu.be/uK2WbyJx-c4', '_blank');
 });
-
-function updateTimer() {
-  let hours = parseInt(document.getElementById('hours').textContent);
-  let minutes = parseInt(document.getElementById('minutes').textContent);
-  let seconds = parseInt(document.getElementById('seconds').textContent);
-  seconds--;
-  if (seconds < 0) {
-    seconds = 59;
-    minutes--;
-    if (minutes < 0) {
-      minutes = 59;
-      hours--;
-      if (hours < 0) {
-        clearInterval(timerInterval);
-        return;
-      } else {
-        document.getElementById('hours').textContent = String(hours).padStart(2, '0');
-      }
-    }
-    document.getElementById('minutes').textContent = String(minutes).padStart(2, '0');
-  }
-  document.getElementById('seconds').textContent = String(seconds).padStart(2, '0');
-}
-const timerInterval = setInterval(updateTimer, 1000);
-updateTimer();
